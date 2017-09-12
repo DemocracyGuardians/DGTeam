b1d114b36f378d46500b0484f3bc41351e13424b
JonFerraiolo <jonferraiolo@gmail.com>
2017-09-11 17:54:03 -0700

    remove unnecessary and maybe problematic calls to req.session.resave at login and logout

--- server/routes/sessionRoutes.js
+++ server/routes/sessionRoutes.js
@@ -115,6 +115,7 @@ exports.login = function(req, res, next) {
             console.log('Before calling session login.  User='+user);
             console.dir(user);
             console.log('req.session.id='+req.session.id);
+            req.session.user = user;
             // regenerate and resave are likely overkill but login/logout don't happen often
             req.session.regenerate(function(err){
               if (err){
@@ -123,19 +124,9 @@ exports.login = function(req, res, next) {
                 res.send(500, { msg, error: UNSPECIFIED_SYSTEM_ERROR })
               } else {
                 console.log('login after regenerate req.session.id='+req.session.id);
-                req.session.user = user;
-                req.session.save(function(err) {
-                  if (err){
-                    let msg = "session save failure for email '" + email + "'";
-                    console.error(msg + ", error= ", err);
-                    res.send(500, { msg, error: UNSPECIFIED_SYSTEM_ERROR })
-                  } else {
-                    let msg = "Login success for email '" + email + "'";
-                    console.log(msg);
-                    console.log('req.session.id='+req.session.id);
-                    res.send({ msg, user })
-                  }
-                });
+                let msg = "Login success for email '" + email + "'";
+                console.log(msg);
+                res.send({ msg, user })
               }
             });
           } else {
@@ -153,25 +144,17 @@ exports.logout = function(req, res, next) {
   console.dir(req.body);
   console.log('req.session.id='+req.session.id);
   req.session.user = null;
-  // regenerate and resave are likely overkill but login/logout don't happen often
+  // regenerate is likely overkill but login/logout don't happen often
   req.session.regenerate(function(err){
     if (err){
       let msg = "session regenerate failure for logout";
       console.error(msg + ", error= ", error);
       res.send(500, { msg, error: UNSPECIFIED_SYSTEM_ERROR })
     } else {
-      req.session.save(function(err) {
-        if (err){
-          let msg = "logout session save failure";
-          console.error(msg + ", error= ", err);
-          res.send(500, { msg, error: UNSPECIFIED_SYSTEM_ERROR })
-        } else {
-          let msg = "Logout success";
-          console.log(msg);
-          console.log('req.session.id='+req.session.id);
-          res.send({ msg })
-        }
-      });
+      let msg = "Logout success";
+      console.log(msg);
+      console.log('req.session.id='+req.session.id);
+      res.send({ msg })
     }
   });
 }
